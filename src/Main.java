c731b581ffe0b33cf3ebb0472fe099b197495fd8
danielsanchez2020 <danielsanchez.2020.ds@gmail.com>
2022-10-26 17:57:50 -0400

    Add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+
+import java.util.Arrays;
+
+import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
+        /*
+        String cadena;
+        Scanner lecturaTeclado = new Scanner(System.in);
+        System.out.print("Escriba la frase: ");
+        cadena = lecturaTeclado.nextLine();
+*/
+        String[] ar = {"Wait","for","me"};
+        Arrays.sort(ar, Collections.reverseOrder());
+        //Arrays.sort(ar);
+        System.out.printf("Modified ar[] Ascendente orden: %s", Arrays.toString(ar));
+        //System.out.println();
+
+
         System.out.println("Hola Mundo!");
     }
 }

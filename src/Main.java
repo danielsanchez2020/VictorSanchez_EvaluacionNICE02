6392d33dc56438844fa7f7bd38831d28ebd0cb64
danielsanchez2020 <danielsanchez.2020.ds@gmail.com>
2022-10-26 09:36:39 -0400

    Add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        Personas per = new Personas();
+        per.nombre = "Victor";
+        System.out.println(per.nombre);
     }
 }
---
+++ src/Personas.java
@@ -0,0 +1,3 @@
+public class Personas {
+    String nombre;
+}
